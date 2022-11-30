ac85a2a5c448fed86a389718f52798c1f3253f4e
Marcel Marin <marcelmarin256@gmail.com>
2022-11-30 16:19:16 -0400

    Ciclo de vida de los componentes y todas sus funciones ⚒

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,55 @@
 import { Component } from '@angular/core';
+import { Product } from './models/product.model';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-store';
-  imgParent = '';
+	title = 'my-store';
+	imgParent = '';
+	products: Product[] = [
+		{
+			id: '1',
+			name: 'Camiseta',
+			image: 'https://randomwordgenerator.com/img/picture-generator/52e9d44b495bac14f1dc8460962e33791c3ad6e04e507441722978d6944fc3_640.jpg',
+			price: 80000,
+		},
+		{
+			id: '2',
+			name: 'Bicicleta casi nueva',
+			price: 356,
+			image: 'https://static.platzi.com/media/user_upload/bike-143dcfe9-3190-49fd-88f7-d3bf74182072.jpg'
+		},
+		{
+			id: '3',
+			name: 'Colleción de albumnes',
+			price: 34,
+			image: 'https://static.platzi.com/media/user_upload/books-80160e05-d177-420b-89c5-01a97b2bdb76.jpg'
+		},
+		{
+			id: '4',
+			name: 'Mis libros',
+			price: 23,
+			image: 'https://static.platzi.com/media/user_upload/album-6f4213d5-1d2d-4e0f-96fe-edb36c3255b4.jpg'
+		},
+		{
+			id: '5',
+			name: 'Casita michi',
+			price: 125,
+			image: 'https://static.platzi.com/media/user_upload/house-034b0c04-eeff-42fa-b506-79f18f73ff90.jpg'
+		},
+		{
+			id: '6',
+			name: 'Lentes vintage',
+			price: 82,
+			image: 'https://static.platzi.com/media/user_upload/glasses-05350737-5831-4c98-be55-824399206dba.jpg'
+		},
+	];
 
-  onLoaded(img: string) {
-    console.log('Image loaded! del padre', img);
-  }
+	onLoaded(img: string) {
+		console.log('Image loaded! del padre', img);
+	}
 }
 
--- src/app/components/img/img.component.ts
+++ src/app/components/img/img.component.ts
@@ -1,24 +1,55 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges, AfterViewInit, OnDestroy } from '@angular/core';
 
 @Component({
-  selector: 'app-img',
-  templateUrl: './img.component.html',
-  styleUrls: ['./img.component.scss']
+	selector: 'app-img',
+	templateUrl: './img.component.html',
+	styleUrls: ['./img.component.scss']
 })
-export class ImgComponent {
+export class ImgComponent implements OnInit, OnChanges, AfterViewInit, OnDestroy {
 
-  @Input() img: string = '';
-  // El parametro <string> lo que hace es darle significado  a que tipo de valor vamos a enviar. En caso de que el valor a enviar no sea un string, entonces el parametro <string> es opcional. Si no se le asigna ningun tipo de valor, entonces el parametro es de tipo any.
-  @Output() loaded = new EventEmitter<string>()
-  // https://www.w3schools.com/w3css/img_lights.jpg
-  imageDefault = 'https://www.m2crowd.com/core/i/placeholder.png';
+	@Input() img: string = '';
+	// El parametro <string> lo que hace es darle significado  a que tipo de valor vamos a enviar. En caso de que el valor a enviar no sea un string, entonces el parametro <string> es opcional. Si no se le asigna ningun tipo de valor, entonces el parametro es de tipo any.
+	@Output() loaded = new EventEmitter<string>()
+	// https://www.w3schools.com/w3css/img_lights.jpg
+	imageDefault = 'https://www.m2crowd.com/core/i/placeholder.png';
 
-  imgError() {
-    this.img = this.imageDefault;
+	constructor() {
+    //before and during render
+    //No async -- once time
+		console.log('contructor','imgValue =>',this.img);
+	}
+
+  ngOnChanges() {
+    //before render
+    //No async -- many times
+    console.log('ngOnChanges','imgValue =>',this.img);
   }
 
-  imgLoaded() {
-    console.log('Image loaded! del hijo');
-    this.loaded.emit('Esto es un string que voy a enviar y la url de la imagen '+this.img);
+  ngOnInit() {
+    //after render
+    //async -- once time
+    console.log('ngOnInit','imgValue =>',this.img);
   }
+
+  ngAfterViewInit() {
+    //after render
+    // handle children
+    //async -- once time
+    console.log('ngAfterViewInit');
+  }
+
+  ngOnDestroy() {
+    //delete
+    //async -- once time
+    console.log('ngOnDestroy');
+  }
+
+	imgError() {
+		this.img = this.imageDefault;
+	}
+
+	imgLoaded() {
+		console.log('Image loaded! del hijo');
+		this.loaded.emit('Esto es un string que voy a enviar y la url de la imagen '+this.img);
+	}
 }
