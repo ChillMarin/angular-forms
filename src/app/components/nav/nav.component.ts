c886187cdbb85c1cb82f22c6ae8eac953fe6b58b
Marcel Marin <marcelmarin256@gmail.com>
2022-12-28 14:44:02 -0400

    implementando router link y router active en el nav para nevegar entre categorias, se inserto adicionalmente un servicio para llamar a las categorias

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,31 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
-import { StoreService } from '../../services/store.service'
+import { StoreService } from '../../services/store.service';
+import { CategoriesService } from '../../services/categories.service';
 
 import { switchMap } from 'rxjs/operators';
 
 import { User } from '../../models/user.model';
+import { Category } from '../../models/category.model';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.scss']
+  styleUrls: ['./nav.component.scss'],
 })
 export class NavComponent implements OnInit {
-
   activeMenu = false;
   counter = 0;
   profile: User | null = null;
+  categories: Category[] = [];
 
   constructor(
     private storeService: StoreService,
     private authService: AuthService,
-  ) { }
+    private categoriesService: CategoriesService
+  ) {}
 
   ngOnInit(): void {
-    this.storeService.myCart$.subscribe(products => {
+    this.storeService.myCart$.subscribe((products) => {
       this.counter = products.length;
     });
+    this.getAllCategories();
   }
 
   toggleMenu() {
@@ -39,10 +43,16 @@ export class NavComponent implements OnInit {
     //   console.log(this.token);
     //   this.getProfile();
     // });
-    this.authService.loginAndGet('jamon@mail.com', '12345678')
-    .subscribe(user => {
-      this.profile = user;
-    });
+    this.authService
+      .loginAndGet('jamon@mail.com', '12345678')
+      .subscribe((user) => {
+        this.profile = user;
+      });
   }
 
+  getAllCategories() {
+    this.categoriesService.getAll().subscribe((categories) => {
+      this.categories = categories;
+    });
+  }
 }
