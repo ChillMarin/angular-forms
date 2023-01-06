0296cefd7c9990b7affed500e9f69d504f95b41b
Marcel Marin <marcelmarin256@gmail.com>
2023-01-06 12:17:48 -0400

    Creacion del website module, donde coloco todo lo que tiene que ver con el cliente

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { LayoutComponent } from './website/components/layout/layout.component';
-import { HomeComponent } from './website/pages/home/home.component';
-import { NotFoundComponent } from './website/pages/not-found/not-found.component';
-import { CategoryComponent } from './website/pages/category/category.component';
-import { MycartComponent } from './website/pages/mycart/mycart.component';
-import { LoginComponent } from './website/pages/login/login.component';
-import { RegisterComponent } from './website/pages/register/register.component';
-import { RecoveryComponent } from './website/pages/recovery/recovery.component';
-import { ProductDetailComponent } from './website/pages/product-detail/product-detail.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
   // Esto es lo que hace es una redirección a la ruta home si el path es vacio
@@ -18,42 +10,12 @@ const routes: Routes = [
   //   redirectTo: 'home',
   //   pathMatch: 'full'
   // },
+
+  // llamamos el modulo de cms y esto lo que nos habilita poder hacer lazyLoading y Code Splitting
   {
     path: '',
-    component: LayoutComponent,
-    children: [
-      {
-        path: '',
-        component: HomeComponent,
-      },
-      {
-        //:id es un parámetro que se puede pasar a la ruta para validar el id de la categoria que queremos mostrar
-        path: 'category/:id',
-        component: CategoryComponent,
-      },
-      {
-        path: 'mycart/:id',
-        component: MycartComponent,
-      },
-      {
-        path: 'login',
-        component: LoginComponent,
-      },
-      {
-        path: 'register',
-        component: RegisterComponent,
-      },
-      {
-        path: 'recovery',
-        component: RecoveryComponent,
-      },
-      {
-        path: 'product/:id',
-        component: ProductDetailComponent,
-      },
-    ]
+    loadChildren: () => import('./website/website.module').then(m => m.WebsiteModule),
   },
-  // llamamos el modulo de cms y esto lo que nos habilita poder hacer lazyLoading y Code Splitting
   {
     path: 'cms',
     loadChildren: () => import('./cms/cms.module').then(m => m.CmsModule),
---
+++ src/app/website/website-routing.module.ts
@@ -0,0 +1,55 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { LayoutComponent } from './components/layout/layout.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CategoryComponent } from './pages/category/category.component';
+import { MycartComponent } from './pages/mycart/mycart.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { RecoveryComponent } from './pages/recovery/recovery.component';
+import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        component: HomeComponent,
+      },
+      {
+        //:id es un parámetro que se puede pasar a la ruta para validar el id de la categoria que queremos mostrar
+        path: 'category/:id',
+        component: CategoryComponent,
+      },
+      {
+        path: 'mycart/:id',
+        component: MycartComponent,
+      },
+      {
+        path: 'login',
+        component: LoginComponent,
+      },
+      {
+        path: 'register',
+        component: RegisterComponent,
+      },
+      {
+        path: 'recovery',
+        component: RecoveryComponent,
+      },
+      {
+        path: 'product/:id',
+        component: ProductDetailComponent,
+      },
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class WebsiteRoutingModule {}
