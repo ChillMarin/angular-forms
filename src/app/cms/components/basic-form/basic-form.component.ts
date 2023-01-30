371615948648eb9cbd4e5d3455f13effcb322d6d
Marcel Marin <marcelmarin256@gmail.com>
2023-01-30 12:17:34 -0400

    Manejo y binding de selects y selects múltiples

--- src/app/cms/components/basic-form/basic-form.component.ts
+++ src/app/cms/components/basic-form/basic-form.component.ts
@@ -10,6 +10,21 @@ export class BasicFormComponent implements OnInit {
 
   // Esto es un controlador de formulario, que se encarga de manejar el estado de un campo de formulario
   nameField = new FormControl('Soy un control o valor por default');
+  emailField = new FormControl('');
+  phoneField = new FormControl('');
+  colorField = new FormControl('#000000');
+  dateField = new FormControl('');
+  monthField = new FormControl('');
+  ageField = new FormControl(12);
+  passwordField = new FormControl('');
+  priceField = new FormControl('50');
+  weekField = new FormControl('');
+  timefield = new FormControl('');
+  searchField = new FormControl('');
+  descriptionField = new FormControl('');
+
+  categoryField = new FormControl('category-4');
+  tagsField = new FormControl(['tag-1', 'tag-3']);
 
   ngOnInit(): void {
     // El controlador de formulario tiene un observable que se dispara cada vez que cambia el valor del campo
