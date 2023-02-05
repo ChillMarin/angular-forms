c7a57b2bfc57409076182aba93aa48331215c884
Marcel Marin <marcelmarin256@gmail.com>
2023-02-05 09:06:49 -0400

    Ajustes para mostrar una barra de progreso indeterminada

--- src/app/cms/pages/category/category.component.ts
+++ src/app/cms/pages/category/category.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CategoriesService } from 'src/app/services/categories.service';
-import { map } from 'rxjs';
+import { map, Observable, BehaviorSubject } from 'rxjs';
 //firebase
 import {
   Storage,
@@ -11,6 +11,7 @@ import {
   listAll,
   getDownloadURL,
   StorageReference,
+  uploadBytesResumable,
 } from '@angular/fire/storage';
 
 @Component({
@@ -20,7 +21,11 @@ import {
 })
 export class CategoryComponent implements OnInit {
   form!: FormGroup;
-  progressBar=0;
+  mostrarBarra = false;
+  progressBar = 0;
+  status = false;
+  progresbarObs = new BehaviorSubject<number>(0);
+  progresbarObs$ = this.progresbarObs.asObservable();
 
   constructor(
     private formBuilder: FormBuilder,
@@ -59,38 +64,73 @@ export class CategoryComponent implements OnInit {
     const image = event.target.files[0];
     const name = image.name;
     //const ref = this.storage.ref(name);
-    
   }
 
   uploadFile2(event: any) {
+    this.mostrarBarra=true;
+
     const image = event.target.files[0];
     const name = image.name;
 
     //ubicacion de a deonde vamos a subir esa imagen, (Primer parametro es la conexion de db, 2do parametro a donde vamos a subir la imagen, si no hay un carpeta images, el la crea)
     const imgRef = ref(this.storage, `imagenes/${name}`);
     // en donde se va subir, y cual es la imagen a subir y listo
-    const task = uploadBytes(imgRef, image);
+    const task = uploadBytesResumable(imgRef, image);
+    //pruebas
+    // para ver el progreso de la subida
+    task.on(
+      'state_changed',
+      (snapshot) => {
+        // en el snapshot podemos ver el progreso de la subida
+        const progreso =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          this.updateprogress(progreso);
+        console.log('Upload is ' + progreso+ '% done');
+        switch (snapshot.state) {
+          case 'paused':
+            console.log('Upload is paused');
+            break;
+          case 'running':
+            this.progressBar =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            console.log('Upload is running');
+            break;
+        }
+      },
+      (error) => {
+        // Handle unsuccessful uploads
+      },
+      () => {
+        // Handle successful uploads on complete
+        // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+        this.status=true;
+        this.progressBar=100;
+        getDownloadURL(task.snapshot.ref).then((downloadURL) => {
+          console.log('File available at', downloadURL);
+        });
+      }
+    );
 
     // visualizamos si nos dio un error o salio todo bien
-    task
-      .then((response) => {
-        console.log(response);
-        this.getImage(name);
-      })
-      .catch((error) => console.log(error));
+    // task
+    //   .then((response) => {
+    //     console.log(response);
+    //     this.getImage(name);
+    //   })
+    //   .catch((error) => console.log(error));
   }
 
   getImage(nameImage: string) {
     // aqui le decimos que nos busque en la url que queremos es decir en el nombre de la carpeta
     const imgRef = ref(this.storage, 'imagenes');
     // con el listAll nos devuelve un array de objetos
-    listAll(imgRef).then(async rta => {
+    listAll(imgRef).then(async (rta) => {
       //console.log(rta);
       //obtenemos el array y buscamos el item que estamos necesitamos
       const itemActual: StorageReference | undefined = rta.items.find(
         (item) => item.name == nameImage
       );
-        // si existe el item dame la url del item
+      // si existe el item dame la url del item
       if (itemActual) {
         // obtengo el link de descarga de esa imagen
         const url = await getDownloadURL(itemActual);
@@ -106,5 +146,9 @@ export class CategoryComponent implements OnInit {
 
   get imageField() {
     return this.form.get('image');
+  }
+
+  updateprogress(progreso:number){
+    this.progressBar=progreso;
   }
 }
